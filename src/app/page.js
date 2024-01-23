b9409d046038c55ec824b588201889d52837d935
starkoka <j22437@inc.kisarazu.ac.jp>
2024-01-23 23:43:52 +0900

    feat:tailwindCSS完全に理解した

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 
 export default function Home() {
   return (
-      <div>
-          <h1>kokastarのHPへようこそ！</h1>
-          <p>とりあえずHTML直打ちからnext.jsに移行したよ(2023/1/22 12:00)</p>
-          <p><a href="https://github.com/starkoka">GitHub</a>にまともなプロフィールがあります。</p>
+      <div className="h-screen w-full bg-gradient-to-br from-blue-300 via-indigo-300 to-purple-300">
+          <nav className="bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg">
+              <h1 className="text-xl m-3">kokastar.dev</h1>
+          </nav>
+          <div className="h-screen w-full bg-gradient-to-br from-blue-300 via-indigo-300 to-purple-300 flex items-center justify-center">
+              <div className="h-96 w-80 bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg pt-10">
+                <p className="text-center">TailWind-CSS完全に理解した</p>
+              </div>
+          </div>
       </div>
   );
 }
