e387fd3f7335fb74c6ed155dd988f2bfe5f71362
starkoka <j22437@inc.kisarazu.ac.jp>
2024-02-07 23:47:17 +0900

    feat:ボタンを右上に寄せた(ちょっと右上すぎるので後日修正)

--- src/app/page.js
+++ src/app/page.js
@@ -74,7 +74,7 @@ export default function Home() {
                   </li>
               </ul>
           </nav>
-          <button className="z-50 space-y-2 md:hidden" onClick={handleMenuOpen}>
+          <button className="z-50 space-y-2 md:hidden ml-auto" onClick={handleMenuOpen}>
               <span
                   className={
                       isOpen
