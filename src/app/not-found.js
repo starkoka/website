90f507ad5bf8c94223c23d3fc9e97e27da7d7354
starkoka <j22437@inc.kisarazu.ac.jp>
2024-05-15 10:25:04 +0900

    feat:404ページのスタイルを少し調整

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -14,8 +14,9 @@ export default function Home() {
                         <div
                             className="h-screen w-full from-blue-300 via-indigo-300 to-purple-300 flex items-center justify-center">
                             <div
-                                className="h-96 w-80 bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg pt-10">
-                                <p className="text-center">404 not found</p>
+                                className="h-96 w-80 bg-gray-200/30 backdrop-blur-lg rounded-md border border-gray-200/30 shadow-lg pt-10 items-center justify-center">
+                                <h1 className="text-center font-bold text-4xl">404 not found</h1>
+                                <p className="text-center">ここに表示するものは何もない！</p>
                             </div>
                         </div>
                     </div>
