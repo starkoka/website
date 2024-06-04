1297ed70481d009b7b20c1429b67f79501c5872c
starkoka <j22437@inc.kisarazu.ac.jp>
2024-06-04 18:49:53 +0900

    feat:トップページもちょっと更新

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,11 @@ export default function Home() {
   return (
       <div>
           <h1>kokastarのHPへようこそ！</h1>
-          <p>とりあえずHTML直打ちからnext.jsに移行したよ(2023/1/22 12:00)</p>
           <p><a href="https://github.com/starkoka">GitHub</a>にまともなプロフィールがあります。</p>
+
+          <h2>----更新情報----</h2>
+          <p>とりあえずHTML直打ちからnext.jsに移行したよ(2023/1/22 12:00)</p>
+          <p>GitHubプロフィール用API作ったよ、正式サイトも着々進めてるよ(2023/6/4 19:00)</p>
       </div>
   );
 }
